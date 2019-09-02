5f80a3477499014d61aad75d3d62fe0a4c577542
wh1rl <whirls.dev@gmail.com>
2019-09-02 10:29:57 +0300

    flutter_linkify web.

--- lib/flutter_linkify.dart
+++ lib/flutter_linkify.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_web/gestures.dart';
+import 'package:flutter_web/material.dart';
 import 'package:linkify/linkify.dart';
 
 export 'package:linkify/linkify.dart'
